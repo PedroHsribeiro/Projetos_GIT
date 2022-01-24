d11fb858be2f87ddb929f1a069aeb98c121320b0
Pedro <pedro.hsribeiro@hotmail.com>
2022-01-24 13:23:29 -0300

    Atualização 001

--- resolucao-problemas/index.js
+++ resolucao-problemas/index.js
@@ -11,15 +11,18 @@ console.log("Seja muito bem-vindo ao sistema de cadastro de Eventos.")
 userage = insertdata("Qual sua idade? ")
 if (userage < 18){
     console.log("Lamento, pessoas menores de idade não podem cadastrar eventos.")
+    return
 }
 eventname = insertdata("Qual o nome do evento que deseja cadastrar? ")
 eventdate = new Date(insertdata("Qual a data do evento? (Insira no formato mm/dd/aaaa) "))
 if (eventdate <= todaydate){
     console.log("Não é possível registrar o evento para hoje ou a data selecionada é anterior à hoje.")
+    return
 }
 participants = insertdata("Qual a quantidade de participantes do evento? ")
 if (participants > 100){
     console.log("Não é possível realizar o cadastro, a quantidade de participantes excede a quantidade máxima de 100.")
+    return
 }
 speakers = insertdata("Quantos palestrantes terá o evento? ")
 
