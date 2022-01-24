d40691160cecf27ef45e54c1dc8527dd2980ade5
Pedro <pedro.hsribeiro@hotmail.com>
2022-01-24 17:27:16 -0300

    atualizacao 002

--- CadastroPecas/cadastropecas.js
+++ CadastroPecas/cadastropecas.js
@@ -5,13 +5,17 @@ let piecename
 let boxopieces 
 
 console.log("Bem vindo ao sistema de cadastro de peças.")
+
 piecename = insertdata("Qual o nome da peça que deseja cadastrar?\n ")
-if (piecename.length < 3){
-    console.log("O nome da peça deve possuir, no mínimo, 4 caracteres.")
+while (piecename.length < 3){
+    piecename = insertdata("O nome da peça deve possuir, no mínimo, 4 caracteres. Insira o nome da peça novamente: \n")
 }
+
 pieceweight = insertdata("Qual o peso da peça? \n")
 if (pieceweight > 100){
-    console.log("Peças com menos de 100g não podem ser cadastradas. ")
+    console.log("Peças com mais de 100g não podem ser cadastradas. ")
+    return
 }
+
 boxopieces = insertdata("Quantas peças deseja cadastrar? \n")
 console.log("Peças cadastradas com sucesso.\n Quantidade de caixas necessárias: ",Math.ceil((boxopieces/10)))
